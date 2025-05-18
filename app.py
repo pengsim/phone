1a735f75db2acf6ad651dce1beaf12db516479e7
pengsim <hghvb024@gmail.com>
2025-05-18 17:17:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def edit_phone(id):
     return jsonify({'message': 'Updated successfully'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' port=5000)
+    app.run(host='0.0.0.0' ,port=5000)
